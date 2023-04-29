de74f563597c434d9e0042e0434ef93d15ace5f3
Drustan <drustanyjt@gmail.com>
2023-04-29 22:55:50 +0800

    Colorized button

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <div className='fixed flex flex-col top-0 left-0 h-screen w-screen justify-center items-center'>
+      <div className='flex flex-row'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -19,8 +19,8 @@ function App() {
       <h1>Vite + React</h1>
       <p>Made by <b>Drustan</b></p>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button className=' bg-slate-400 px-2 py-1 rounded-md hover:bg-green-300 hover:text-green-700' onClick={() => setCount((count) => count + 1)}>
+          Count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
@@ -29,7 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
