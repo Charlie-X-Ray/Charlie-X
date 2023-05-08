34eaf1e569141adbf9417074106a5ab9b66f4d48
Drustan <drustanyjt@gmail.com>
2023-05-08 11:28:13 +0800

    testing workflows

--- src/commons/ErrorPage.jsx
+++ src/commons/ErrorPage.jsx
@@ -5,7 +5,7 @@ export default function ErrorPage() {
   return (
     <div className="flex h-screen w-screen justify-center items-center">
       <Link to={`/`}>
-        <button className="px-2 py-1 bg-green-600 hover:bg-green-800">
+        <button data-testid="error-return-button" className="px-2 py-1 bg-green-600 hover:bg-green-800">
           Go home
         </button>
       </Link>
