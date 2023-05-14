cf05f2d5c535107b0df63ff1db36bf85be660a87
Drustan <drustanyjt@gmail.com>
2023-05-14 16:40:11 +0900

    Changed title of about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import SubpageLayout from "../commons/SubpageLayout";
 function About() {
 
   return (
-    <SubpageLayout heading="About">
+    <SubpageLayout heading="About Charlie X">
       <div className="flex h-screen items-center justify-center">
         <h1 className="bg-red-300">
           This is the about page!
