8a4bc132811b8426cfec987004d9a8fd9e6484ac
Drustan <drustanyjt@gmail.com>
2023-05-17 16:05:43 +0900

    Added search bar to browse page

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -8,14 +8,15 @@ const GalleryHero = ({ image, desc, ...props }) => {
 
   return (
     // Why wont this pass key with props drilling?
-    <div className="my-4 h-80 w-11/12 flex flex-col items-center font-mono font-semibold border-2 bg-blue-300 text-white" {...props}>
-      <div className="flex h-1/4 items-center shrink-0 text-lg md:text-xl lg:text-2xl whitespace-nowrap">
-        <Link to={`/`}>
+    <div className="my-4 w-11/12 flex flex-col items-center font-mono font-semibold border-0
+    bg-[#BAE5E3] text-black rounded-t-2xl" {...props}>
+      <div className="w-full shrink-0 text-md md:text-md lg:text-md whitespace-nowrap">
+        <Link className="pl-3" to={`/`}>
           {desc}
         </Link>
       </div>
-      <div className="flex mb-3 h-3/4 w-full justify-center items-center text-6xl ">
-        <Link className="h-3/4 w-3/4 hover:h-5/6 hover:w-5/6" to={`/`}>
+      <div className="flex h-3/4 w-full justify-center items-center text-6xl ">
+        <Link className="h-full w-full" to={`/`}>
           {image}
         </Link>
       </div>
@@ -35,19 +36,46 @@ function Gallery({ heroProps }) {
   )
 }
 
+const SearchBar = ( { value, setValue }) => {
+
+  const letters = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y', 'Z',
+  ]
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+
+      <div className="flex gap-3">
+        {letters.map((l, i) => {
+          return (
+            <button key={i} onClick={() => {
+              setValue(l);
+            }}>{l}</button>
+          )
+        })}
+      </div>
+      <input
+        className="max-w-lg w-3/4 bg-slate-600 pl-2 text-white"
+        placeholder="Search"
+        value={value}
+        onChange={(e) => setValue(e.target.value)} />
+
+    </div>
+  )
+}
+
 const Browse = () => {
 
   const [ explore, setExplore ] = useState("");
 
-  const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+  const testArray = ["Atelectasis", "Alveolar Nodule", "Asperigillosis", "Disease", "Diseased", "Sickly", "Super duper sick"]
 
   return (
     <SubpageLayout heading="Browse">
       <div className="flex w-full justify-center p-2 mt-4">
-        <ExploreBar
-          value = { explore }
-          onChange = { e => setExplore(e.target.value)}
-        />
+        <SearchBar value={ explore } setValue={ setExplore } />
       </div>
       <Gallery heroProps={testArray.filter(x => x.toString().includes(explore)).map(x => {
         return {
