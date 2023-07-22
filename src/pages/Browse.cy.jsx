bdc118c4f0632373f8c8be90c934f57e6d6dae52
Drustan <drustanyjt@gmail.com>
2023-07-22 11:08:55 +0800

    increased timeout, combined xray loads into one

--- src/pages/Browse.cy.jsx
+++ src/pages/Browse.cy.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Browse from './Browse'
 
 describe('<Browse />', () => {
-  const timeout = 30000
+  const timeout = 60000
   beforeEach('renders', () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(<Browse />)
@@ -11,13 +11,13 @@ describe('<Browse />', () => {
     cy.root().contains('Browse')
   })
 
-  it.only('displays at least 10 xrays', () => {
+  it.only('displays xrays and can filter them', () => {
     // Should find condition names
-    cy.get('[data-test="xrayhero"]', {timeout: timeout})
-      .should('have.length.at.least', 10)
+  //   cy.get('[data-test="xrayhero"]', {timeout: timeout})
+  //     .should('have.length.at.least', 10)
 
-  })
-  it.only('has a usable search bar for filtering', () => {
+  // })
+  // it.only('has a usable search bar for filtering', () => {
     const searchStr = "cardio"
     const negativeSearchStr = "ILD"
     cy.get('[data-test="xrayhero"]', {timeout: timeout})
@@ -34,8 +34,8 @@ describe('<Browse />', () => {
     cy.get('[data-test="xrayhero"]', {timeout: timeout})
       .as('xrays')
       .should('not.have', negativeSearchStr)
-  })
-  it.only('has modal x rays', () => {
+  // })
+  // it.only('has modal x rays', () => {
     cy.get('[data-test="xrayhero"]')
       .first()
       .click()
